1a6e31645faceeee801897c3203d634ccb1daff2
GabrielFong15 <131215356+GabrielFong15@users.noreply.github.com>
2023-05-11 14:04:44 +1000

    score + door anim fixed

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,35 +6,29 @@ using UnityEngine.UI;
 public class LevelManager : MonoBehaviour
 {
     private int highScore;
-    [HideInInspector] public int score;
-    [HideInInspector] public float health;
-    [HideInInspector] public int ammo;
-    public float damageMultiplyer = 1.0f;
+    public int score;
+
     public Text highScoreText;
     public Text scoreText;
-    public Text healthText;
-    public Text ammoText;
-    public GameObject playerGun;
-    public Gun pg;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         highScore = PlayerPrefs.GetInt("HIGHSCORE");
         highScoreText.text = "High Score: " + highScore;
-        playerGun = GameObject.Find("Gun");
-        pg = playerGun.gameObject.GetComponent<Gun>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        health = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDamage>().health;
-        ammo = pg.ammoCount;
+
         scoreText.text = "Score: " + score;
-        healthText.text = "Health: " + health;
-        ammoText.text = "Ammo: " + ammo;
+
 
         if (highScore < score)
         {
